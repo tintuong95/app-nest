4369cb6960e5730b3ee5ae9e8024d82034690c30
tintuong <tintuong95@gmail.com>
2023-05-27 15:50:33 +0700

    d

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! tin tuong dep trai';
+    return 'Hello World! tin tuong dep trai1';
   }
 }
