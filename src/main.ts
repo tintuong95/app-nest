05c5276ac8aac4e55c3f8cd50bb4e44fd1d4210a
tintuong <tintuong95@gmail.com>
2023-05-27 15:22:27 +0700

    d

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
+  console.log(port);
   await app.listen(port);
 }
 bootstrap();
